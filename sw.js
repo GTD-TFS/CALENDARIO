5d98c07f7dcf1d8bafe35fb1d079ef30d6c9b1cf
GTD-TFS <adeje.gtd@hotmail.com>
2026-01-15 00:40:36 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,35 @@
-const CACHE = "cal-local-v2";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
+const CACHE = "cal-local-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./firebase.js",
+  "./auth.js",
+  "./app.js",
+  "./vacaciones.js",
+  "./admin.js",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", (e)=>{
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 
+self.addEventListener("activate", (e)=>{
+  e.waitUntil((async ()=>{
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))));
+    await self.clients.claim();
+  })());
+});
+
 self.addEventListener("fetch", (e)=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+  // Solo cache-first para tus archivos; lo externo (Firebase CDN) NO se cachea
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
